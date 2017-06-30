e2ef4c343b1c18f80a2bcdff1d9d6aad95d47e8e
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-06-30 16:13:45 -0700

    adds the validations on the rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@ class Review < ActiveRecord::Base
   validates :user, :comment, :rating, :presence => true
   validates_length_of :comment, minimum: 50
   validates_length_of :comment, maximum: 250
-
+  validates_numericality_of :rating, less_than_or_equal_to: 5
+  validates_numericality_of :rating, greater_than_or_equal_to: 1
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,5 +6,7 @@ describe Review do
   it { should validate_presence_of :rating }
   it { should validate_length_of(:comment).is_at_least(50) }
   it { should validate_length_of(:comment).is_at_most(250) }
+  it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
+  it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1) }
   it { should belong_to :product }
 end
