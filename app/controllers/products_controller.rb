85acf140fa6b6d8cb604a951de9d5ed6ec35a912
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-06-30 10:44:07 -0700

    sets up views and controller for new, edit, _form, _errors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,35 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :image, :origin)
+  end
+
 end
