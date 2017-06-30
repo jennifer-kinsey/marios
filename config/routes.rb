700bd44932227b66f9e43ea16fd83e823014b6b0
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-06-30 15:36:45 -0700

    adds the add review to the product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'about#index'
 
+  resources :about, only: [:index]
+
   resources :products do
     resources :reviews, :except => [:show, :index]
   end
